0f26c25a3f7f05a2dc39ed984fbff2b350a19e01
Succo Rolando <48627654+zerocrise@users.noreply.github.com>
2026-01-23 15:06:52 +0100

    Delete Cap14-JList-JTable/src directory

--- Cap14-JList-JTable/src/BibliotecaException.java
+++
@@ -1,9 +0,0 @@
-public class BibliotecaException extends Exception {
-    public BibliotecaException(String messaggio) {
-        super(messaggio);
-    }
-
-    public BibliotecaException(String messaggio, Throwable causa) {
-        super(messaggio, causa);
-    }
-}
